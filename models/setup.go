0a6643e455bee2dc8eb6ea1d8c5f9d33efb66c06
Martin Trejo Chavez <chilli.coder@gmail.com>
2020-07-05 18:59:53 -0500

    Preparacion para generar imagen Docker
      Agrega Dockerfile
      Cambia "dialecto" de BD a MySQL

--- models/setup.go
+++ models/setup.go
@@ -4,13 +4,22 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "test.db")
+	var connection_string, exists = os.LookupEnv("DB_URL")
+
+	if !exists {
+		connection_string = "bookstore:bookstorepwd1@localhost:3306/bookstore_db?charset=utf8&parseTime=True"
+	}
+
+	log.Print(connection_string)
+	database, err := gorm.Open("mysql", connection_string)
 
 	if err != nil {
 		panic("Failed to connect to database!")
